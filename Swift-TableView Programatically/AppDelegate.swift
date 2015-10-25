65331fade6a0e7ca9040790fbc838b20358ea3e0
shanmugasundharamselvadurai <shanmugamselvadurai@gmail.com>
2015-10-25 15:56:46 +0530

    Table view added programmatically and With out storyboard

--- Swift-TableView Programatically/AppDelegate.swift
+++ Swift-TableView Programatically/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Swift-TableView Programatically
 //
-//  Created by Mine on 10/24/15.
+//  Created by shan on 10/24/15.
 //  Copyright (c) 2015 Ximenia. All rights reserved.
 //
 
@@ -13,12 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window?.backgroundColor = UIColor.whiteColor()
+        var viewController = ViewController()
+        var navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
--- Swift-TableView Programatically/ViewController.swift
+++ Swift-TableView Programatically/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
 
+     var myTableView = UITableView()
+    //Array values
+    var tableData: [String] = ["Apple","orange","pinaple"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myTableView = UITableView(frame: CGRect(x: 0,y: 0,width: UIScreen.mainScreen().bounds.width,height:UIScreen.mainScreen().bounds.height))
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        myTableView.backgroundColor = UIColor.clearColor()
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(myTableView)
+        
+    }
+    
+    // MARK: - TableView Delegate
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        cell.textLabel?.text = tableData[indexPath.row]
+        
+        return cell
     }
 
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
